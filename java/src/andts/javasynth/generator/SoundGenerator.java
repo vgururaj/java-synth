614552a65d6a800cba1aebda6083dc391eb7e9f9
a.tsvelodub <a.tsvelodub@gmail.com>
2011-11-23 13:23:19 +0000

    Added some JavaDocs

--- java/src/andts/javasynth/generator/SoundGenerator.java
+++ java/src/andts/javasynth/generator/SoundGenerator.java
@@ -6,6 +6,12 @@ public class SoundGenerator extends Generator<Integer>
 {
     private SamplePreAmplifier amp;
 
+    /**
+     * Creates new SoundGenerator
+     * @param sampleSize size of generated sample in bits
+     * @param osc oscillator used to generate wave of some frequency
+     * @param gain used to amplify generated sample to some volume
+     */
     public SoundGenerator(int sampleSize, Oscillator osc, Gain gain)
     {
         super(osc, gain);
